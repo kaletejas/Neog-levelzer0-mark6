115dd3986f0286ff66e6368f0f7b67d9035eda2b
kaletejas <tejaskale.messi@gmail.com>
2022-09-17 00:43:36 +0530

    feat : input and output

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var btnTrans = document.querySelector("#btn");
-var inputText = document.querySelector("#text");
-
+var inputText = document.querySelector("#textIn");
+var outputText = document.querySelector("#textOut");
 
 btnTrans.addEventListener("click", function addEventListener(){
     console.log("Buttion clicked.");
 } )
 
-inputText.addEventListener("click", function addEventListener(){
-    
-})
