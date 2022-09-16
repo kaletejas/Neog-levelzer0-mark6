34a7688e95341aa3353d5e23027f66a355be74d9
kaletejas <tejaskale.messi@gmail.com>
2022-09-16 22:53:46 +0530

    added files

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTrans = document.querySelector("#btn");
+var inputText = document.querySelector("#text");
+
+
+btnTrans.addEventListener("click", function addEventListener(){
+    console.log("Buttion clicked.");
+} )
+
+inputText.addEventListener("click", function addEventListener(){
+    
+})
