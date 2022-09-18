5a1a9cb543e6e87c0742f5d5ead5a1c22584d1be
kaletejas <tejaskale.messi@gmail.com>
2022-09-18 20:04:33 +0530

    feat : added translation logic

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var btnTrans = document.querySelector("#btn");
 var inputText = document.querySelector("#textIn");
 var outputText = document.querySelector("#textOut");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-btnTrans.addEventListener("click", function addEventListener(){
-    console.log("Buttion clicked.");
-} )
+
+
+function getNewURL(input){
+    return serverURL + "?" + "text=" + input;
+}
+function clickHandler(){
+
+    var input = inputText.value;
+    fetch(getNewURL(input))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputText.innerText = translatedText;   
+    })
+    .catch(errorHandler)
+}
+
+function errorHandler(error){
+    alert(error + "error occured try after sometime." )
+}
+
+btnTrans.addEventListener("click", clickHandler)
 
